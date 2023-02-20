117dc14cf840fd105305c9f2010ddfb5f3f75e8e
f-hansie <maphanga1988@gmail.com>
2023-02-20 13:27:26 +0200

    added space on line of code

--- test/ListAllPosts-spec.js
+++ test/ListAllPosts-spec.js
@@ -14,6 +14,7 @@ describe('Lists all post api', async function () {
             expect(response.status).to.eql(200);
             console.log(response);
           }, (error) => {
+            
             console.log(error);
           });
     });
